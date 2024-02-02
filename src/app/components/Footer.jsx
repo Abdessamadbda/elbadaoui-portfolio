1312f4e8f5ddf76eec7b0c2b980e353f60964258
Abdessamadbda <abdessamadelbada02@gmail.com>
2024-02-02 19:04:01 +0000

    first commit

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+      <img
+      className="h-8 w-16 h-16"
+      src="ea.png"
+      alt="EA"
+    />
+        <p className="text-slate-600">Made with 💖 by Elbadaoui Abdessamad 
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
